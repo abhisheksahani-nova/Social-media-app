78efba18359a6a6d1b41fcf3c9c9d4222bcfa2fd
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-06-06 11:30:20 +0530

    Add state and logic for poll Modal

--- src/components/CreatePollModal/CreatePollModal.jsx
+++ src/components/CreatePollModal/CreatePollModal.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 import "./CreatePollModal.css";
+import TextareaAutosize from "react-textarea-autosize";
 
 function CreatePollModal({ setPollModal }) {
+  const [pollModalOptionNum, setPollModalOptionNum] = useState(2);
+  const [pollData, setPollData] = useState({
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+  });
+
+  function handleAddOption() {
+    setPollModalOptionNum((prev) => prev + 1);
+  }
+
   return (
     <div className="playlist-dropdown-container">
       <div
@@ -17,16 +31,20 @@ function CreatePollModal({ setPollModal }) {
           ></i>
         </div>
 
-        <div className="poll-modal-container">
+        <div className="poll-modalbody-container">
           <div className="d-flex flex-direction-col playlist-li-item cursor-p gap-small mb-small">
             <label className="profile-edit-modal-inp-label">
               Your question
             </label>
-            <input
+
+            <TextareaAutosize
               type="text"
               placeholder="E.g., How do you commute to work?"
               className="profile-edit-modal-inp"
-            ></input>
+              onChange={(e) =>
+                setPollData({ ...pollData, question: e.target.value })
+              }
+            />
           </div>
 
           <div className="d-flex flex-direction-col playlist-li-item cursor-p gap-small mb-small">
@@ -35,6 +53,9 @@ function CreatePollModal({ setPollModal }) {
               type="text"
               placeholder="E.g., Public transportation"
               className="profile-edit-modal-inp"
+              onChange={(e) =>
+                setPollData({ ...pollData, option1: e.target.value })
+              }
             ></input>
           </div>
 
@@ -44,10 +65,48 @@ function CreatePollModal({ setPollModal }) {
               type="text"
               placeholder="E.g., Drive myself"
               className="profile-edit-modal-inp"
+              onChange={(e) =>
+                setPollData({ ...pollData, option2: e.target.value })
+              }
             ></input>
           </div>
 
-          <button className="btn btn-custom-sty btn-custom-small mb-small">Add option</button>
+          {pollModalOptionNum >= 3 && (
+            <div className="d-flex flex-direction-col playlist-li-item cursor-p gap-small mb-small">
+              <label className="profile-edit-modal-inp-label">Option 3</label>
+              <input
+                type="text"
+                placeholder="E.g., Ola or Uber"
+                className="profile-edit-modal-inp"
+                onChange={(e) =>
+                  setPollData({ ...pollData, option3: e.target.value })
+                }
+              ></input>
+            </div>
+          )}
+
+          {pollModalOptionNum == 4 && (
+            <div className="d-flex flex-direction-col playlist-li-item cursor-p gap-small mb-small">
+              <label className="profile-edit-modal-inp-label">Option 4</label>
+              <input
+                type="text"
+                placeholder="E.g., Other"
+                className="profile-edit-modal-inp"
+                onChange={(e) =>
+                  setPollData({ ...pollData, option4: e.target.value })
+                }
+              ></input>
+            </div>
+          )}
+
+          {pollModalOptionNum !== 4 && (
+            <button
+              className="btn pri-outline-btn btn-custom-small mb-small"
+              onClick={() => handleAddOption()}
+            >
+              + Add option
+            </button>
+          )}
         </div>
 
         <div className="d-flex justify-cont-right mt-1">

445b03098b97e0916018ecfc8fa0788da70d7e61
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-07-05 17:23:53 +0530

    Refactor

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -14,8 +14,11 @@ function LandingPage() {
         <div className="d-flex flex-direction-col gap-2 align-item-center justify-cont-center  landingpage-content-container">
           <i className="fa-solid fa-camera camera-icon"></i>
           <h1 className="landingpage-content">
-            Find your tribe on Social. Create, Consume & Share post with others
-            in this simple yet useful social media app.
+            Find your tribe on Social.{" "}
+            <span className="blue-text-clr">Create</span>,{" "}
+            <span className="yellow-text-clr">Consume</span> &{" "}
+            <span className="pink-text-clr">Share</span> post with others in
+            this simple yet useful social media app.
           </h1>
           <button
             className="btn pri-outline-btn"
